2d039cca312abf3c7e72cc2df6596ab979121d14
Marcus Lee <marcustutorial@hotmail.com>
2021-07-18 18:27:08 +0800

    added PD video

---
+++ src/pages/video/pdvideo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <video width="540" height="960" controls>
+      <source
+        src="https://firebasestorage.googleapis.com/v0/b/mentu-lxs.appspot.com/o/PDFootage1.mp4?alt=media"
+        type="video/mp4"
+      />
+    </video>
+  </div>
+</template>
